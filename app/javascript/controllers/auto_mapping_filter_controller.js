7e50394cd8d21c8a4c84ab0c234f697cc751d7ca
Imene-Amirat <imene2004.amirat@gmail.com>
2026-03-12 16:10:15 +0100

    Add empty state message for filtered mappings

--- app/javascript/controllers/auto_mapping_filter_controller.js
+++ app/javascript/controllers/auto_mapping_filter_controller.js
@@ -2,17 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="auto-mapping-filter"
 
 export default class extends Controller {
-  static targets = ["row"]
+  static targets = ["row", "emptyMessage"]
 
   toggle(event) {
     const hide = event.target.checked 
-
     const autoSources = ['LOOM','CUI', 'SAME_URI']
+    let count = 0
 
     this.rowTargets.forEach((row) => {
       const isAutomatic = autoSources.includes(row.dataset.source)   
-
       row.style.display = hide && isAutomatic ? 'none' : ''
+
+      if(row.style.display === 'none') count++
     })
+
+    if(this.hasEmptyMessageTarget) {
+      this.emptyMessageTarget.style.display = count === this.rowTargets.length ? '' : 'none'
+    }
   }
 }
